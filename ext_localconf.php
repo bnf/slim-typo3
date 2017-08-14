c930b422398e1eea0ce5740e3fbb98896bfc86a4
Benjamin Franzke <benjaminfranzke@gmail.com>
2017-08-14 07:08:26 +0200

    ext_localconf: Add some TODOs

--- ext_localconf.php
+++ ext_localconf.php
@@ -3,7 +3,6 @@ if (!defined('TYPO3_MODE')) {
     die('Access denied.');
 }
 
-
 if (TYPO3_REQUESTTYPE & (TYPO3_REQUESTTYPE_FE | TYPO3_REQUESTTYPE_BE | TYPO3_REQUESTTYPE_AJAX)) {
     \TYPO3\CMS\Core\Core\Bootstrap::getInstance()->registerRequestHandlerImplementation(
         \Bnf\SlimTypo3\Http\SlimRequestHandler::class
@@ -14,9 +13,17 @@ $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['slim_typo3']['settings'] = [
     'displayErrorDetails' => (int)$GLOBALS['TYPO3_CONF_VARS']['SYS']['displayErrors'] === 1,
     //'routerCacheFile' => PATH_site . 'typo3temp/Cache/Code/slim_typo3/router-cache.php',
 
-    // set to false if you need no support for \TYPO3\CMS\Extbase\Utility\DebuggerUtility::var_dump()
+    // set to false(bool) if you do not need support for \TYPO3\CMS\Extbase\Utility\DebuggerUtility::var_dump()
+    // within slim routes
     'outputBuffering' => 'prepend',
 ];
 
-
 $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['slim_typo3']['configureApp'][] = \Bnf\SlimTypo3\Example\TestApp::class;
+
+/* TODO: provide an easy to use route/middleware-add interface like (to be used instead of the 'configureApp' Hook:
+\Bnf\SlimTypo3\NameThis::configure(function (\Slim\App $app) {
+    $app->get('/test', \Bnf\SlimTypo3\Example\TestApp::class . ':bar');
+});
+
+Or \Bnf\SlimTypo3\Http\SlimRequestHandler::configure(function (\Slim\App $app) {}); ?
+*/
