26e687446ed44b0035f8cec7319bf8f2b08e44ca
Benjamin Franzke <benjaminfranzke@gmail.com>
2017-08-14 17:32:02 +0200

    ext_emconf.php: Fix version constraint

--- ext_emconf.php
+++ ext_emconf.php
@@ -14,7 +14,7 @@ $EM_CONF[$_EXTKEY] = array(
     'version' => '',
     'constraints' => array(
         'depends' => array(
-            'typo3' => '6.2.0-7.6.99',
+            'typo3' => '8.7.0-8.7.99',
         ),
         'conflicts' => array(
         ),
