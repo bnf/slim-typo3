4591aa0e26e2c061064d30dbcbc80e5907d4bb82
Benjamin Franzke <benjaminfranzke@gmail.com>
2019-08-20 15:55:41 +0200

    Provide PSR-17 HTTP Factories

--- ext_localconf.php
+++ ext_localconf.php
@@ -12,3 +12,14 @@ $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['slim_typo3']['settings'] = [
 
     //'routerCacheFile' => PATH_site . 'typo3temp/Cache/Code/slim_typo3/router-cache.php',
 ];
+
+if (version_compare(TYPO3_version, '10.0', '<')) {
+    $container = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Extbase\Object\Container\Container::class);
+    $container->registerImplementation(\Psr\Http\Message\RequestFactoryInterface::class, \Bnf\Typo3HttpFactory\RequestFactory::class);
+    $container->registerImplementation(\Psr\Http\Message\ResponseFactoryInterface::class, \Bnf\Typo3HttpFactory\ResponseFactory::class);
+    $container->registerImplementation(\Psr\Http\Message\ServerResponseFactoryInterface::class, \Bnf\Typo3HttpFactory\ServerRequestFactory::class);
+    $container->registerImplementation(\Psr\Http\Message\StreamFactoryInterface::class, \Bnf\Typo3HttpFactory\StreamFactory::class);
+    $container->registerImplementation(\Psr\Http\Message\UploadedFileFactoryInterface::class, \Bnf\Typo3HttpFactory\UploadedFileFactory::class);
+    $container->registerImplementation(\Psr\Http\Message\UriFactoryInterface::class, \Bnf\Typo3HttpFactory\UriFactory::class);
+    unset($container);
+}
