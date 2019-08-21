8f287b2659f08da7ba64f28a9efd28e48c98e2dd
Benjamin Franzke <benjaminfranzke@gmail.com>
2019-08-21 10:35:52 +0200

    Fix typo in PSR-17 interface aliases

--- ext_localconf.php
+++ ext_localconf.php
@@ -17,7 +17,7 @@ if (version_compare(TYPO3_version, '10.0', '<')) {
     $container = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Extbase\Object\Container\Container::class);
     $container->registerImplementation(\Psr\Http\Message\RequestFactoryInterface::class, \Bnf\Typo3HttpFactory\RequestFactory::class);
     $container->registerImplementation(\Psr\Http\Message\ResponseFactoryInterface::class, \Bnf\Typo3HttpFactory\ResponseFactory::class);
-    $container->registerImplementation(\Psr\Http\Message\ServerResponseFactoryInterface::class, \Bnf\Typo3HttpFactory\ServerRequestFactory::class);
+    $container->registerImplementation(\Psr\Http\Message\ServerRequestFactoryInterface::class, \Bnf\Typo3HttpFactory\ServerRequestFactory::class);
     $container->registerImplementation(\Psr\Http\Message\StreamFactoryInterface::class, \Bnf\Typo3HttpFactory\StreamFactory::class);
     $container->registerImplementation(\Psr\Http\Message\UploadedFileFactoryInterface::class, \Bnf\Typo3HttpFactory\UploadedFileFactory::class);
     $container->registerImplementation(\Psr\Http\Message\UriFactoryInterface::class, \Bnf\Typo3HttpFactory\UriFactory::class);
